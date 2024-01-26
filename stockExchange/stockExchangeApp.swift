fb60a860c2abbecf6149409dc42b1bab81d2d586
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-01-26 23:26:01 +0500

    Initial Commit

---
+++ stockExchange/stockExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  stockExchangeApp.swift
+//  stockExchange
+//
+//  Created by Enzhe Gaysina on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct stockExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
